2910491aa126d39f9d988fa2cca7d5117274f0c6
yozif-t <yusuf.torkia@cheil.com>
2023-09-24 17:28:26 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Search from './starWarsComponents/js/search';
+import React, { Component, useState } from 'react';
+
+function App() {
+  return (
+    <div className='starWarsWrapper'>
+      <Search />
+    </div>
+  )
+}
+
+export default App;
